6967e9b4a673d373f7822150ef68aaefb02f3cbf
André Abadesso <andre@lab21k.com.br>
2018-05-16 20:18:59 -0300

    feat: added install script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 scp -r opt/* deped@192.168.10.79:/opt/
 scp daemon.js deped@192.168.10.79:/opt/
-scp test_arduino.js deped@192.168.10.79:/opt/
 scp protocol.js deped@192.168.10.79:/opt/
 scp package.json deped@192.168.10.79:/opt/
 scp index.html deped@192.168.10.79:/opt/
 scp -r ./visualizer/web deped@192.168.10.79:/opt/visualizer/
 scp -r ./visualizer/package.json deped@192.168.10.79:/opt/visualizer/
 scp -r ./visualizer/server.js deped@192.168.10.79:/opt/visualizer/
+scp -r ./systemd/record-web.service deped@192.168.10.79:/opt/visualizer/
+scp -r ./systemd/record.service deped@192.168.10.79:/opt/visualizer/
---
+++ record_install.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+# Ubuntu 16.04.4 Desktop 64 bits
+# P&D TV Globo - Alvinho
+
+# Update SO
+sudo apt -y update && sudo apt -y upgrade
+
+# Set timezone to Brazil UCT-3
+sudo timedatectl set-timezone America/Sao_Paulo
+
+# Prepare removable drive for video recording
+# Enable support for ExFAT ,format removable drive, configure automount
+# Plug drive before commands
+sudo apt install -y exfat-fuse exfat-utils
+sudo mkfs.exfat -n VIDEODRIVE /dev/sdb1
+sudo fsck.exfat /dev/sdb1
+mkdir -p /media/videodrive
+echo '/dev/sdb1 /media/videodrive exfat rw,users 0 0' | sudo tee -a /etc/fstab
+
+# Allow a regular user to mount fuse drive
+sudo chmod u+s /sbin/mount.exfat-fuse
+sudo sed -i 's/#user_allow_other/user_allow_other/g' /etc/fuse.conf
+mount -t exfat /dev/sdb1 /media/videodrive
+
+# Install FFMPEG Prerequisites
+sudo apt -y install autoconf build-essential \
+git libasound2-dev libdbus-1-dev libflac-dev \
+libfreetype6-dev libgl1-mesa-dev libjpeg-dev \
+libmpg123-dev libpng-dev libsdl2-dev libsdl2-image-dev \
+libsdl2-mixer-dev libsdl2-ttf-dev libssl-dev \
+libtool libudev-dev libvorbis-dev libx11-dev \
+make mercurial nasm openssl xorg-dev yasm \
+libx264-dev libfdk-aac-dev v4l-utils libv4l-dev \
+htop texinfo libfribidi-dev
+
+# Clone, Compile and install Simple DirectMedia Layer
+cd "${HOME}" || exit
+hg clone https://hg.libsdl.org/SDL SDL
+cd SDL || exit
+make clean
+./autogen.sh
+./configure
+make all -j "$(nproc)"
+sudo make install
+cd test || exit
+make clean
+./configure
+sudo ln -s /usr/include/SDL2/SDL_ttf.h /usr/local/include/SDL2/
+make all -j "$(nproc)"
+
+# Clone, Compile and install FFMPEG
+# Enable support for H264, AAC, video4linux and drawtext
+cd "${HOME}" || exit
+git clone https://github.com/FFmpeg/FFmpeg.git
+cd FFmpeg || exit
+git pull
+./configure \
+	--enable-gpl \
+	--enable-nonfree \
+	--enable-openssl \
+	--enable-libx264 \
+	--enable-libfdk-aac \
+	--enable-libv4l2 \
+	--enable-libfreetype \
+	--enable-libfontconfig \
+	--enable-libfribidi
+make -j "$(nproc)"
+sudo make install
+
+# configure autologon for the user
+# Disable screen blackout and lock
+sudo gsettings set org.gnome.desktop.session idle-delay 0
+sudo gsettings set org.gnome.desktop.screensaver lock-enabled false
+
+# Set permissions for current user to access webcam
+sudo usermod -aG video "$USER"
+sudo usermod -aG audio "$USER"
+sudo usermod -aG pulse "$USER"
+sudo usermod -aG pulse-access "$USER"
+sudo chmod g+rw /dev/video0
+sudo chmod g+rw /dev/video1
+
+# Start pulseaudio as a daemon for headless usage
+echo 'pulseaudio -D' | sudo tee /etc/rc.local
+echo 'exit 0' | sudo tee --append /etc/rc.local 
+
+# Set X display for the user
+# Disable screensaver and screen blanking
+echo "export DISPLAY=:0" >> "${HOME}"/.profile
+echo "xset s off && xset s noblank && xset -dpms" >> "${HOME}"/.profile
+source "${HOME}"/.profile
+
+# Install node.js via NVM
+cd "${HOME}" || exit
+sudo apt install -y build-essential libssl-dev curl
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh -o install.sh
+bash install.sh
+source "${HOME}"/.profile
+nvm install v9.11.1
+
+# Install orchestrator daemon and ffmpeg scripts
+cd "${HOME}"/ffmpeg_dvr || exit
+chmod +x record_cam*
+sudo mkdir /opt/
+git clone https://github.com/andreabadesso/recorder-scripts.git
+sudo chown -R "$USER":"$USER" /opt/
+sudo cp ./recorder-scripts/* /opt/
+
+cd /opt/
+npm install --production
+
+cd /opt/visualizer
+npm install --production
+
+cp /opt/systemd/*.service /lib/systemd/system/
+
+sudo chmod 644 /lib/systemd/system/record.service
+sudo chmod 644 /lib/systemd/system/record-web.service
+
+systemctl enable record-web.service
+systemctl enable record.service
