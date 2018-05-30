67768b678dafd66a386d25d9bca531eb7e656d32
Deped <andre@lab21k.com.br>
2018-05-30 18:05:22 -0300

    feat: added local deploy file

---
+++ deploy_local.sh
@@ -0,0 +1,10 @@
+cp -R opt/* /opt/
+cp daemon.js /opt/
+cp protocol.js /opt/
+cp package.json /opt/
+cp index.html /opt/
+cp -R ./visualizer/web /opt/visualizer/
+cp -R ./visualizer/package.json /opt/visualizer/
+cp -R ./visualizer/server.js /opt/visualizer/
+cp -R ./systemd/record-web.service /opt/visualizer/
+cp -R ./systemd/record.service /opt/visualizer/
